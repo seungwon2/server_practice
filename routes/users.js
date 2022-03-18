f8a72fdec2db1aaad909e0c060cfd6b5faf110bd
seungwon2 <dandelionsw2@ewhain.net>
2022-03-18 10:48:15 +0900

    :building_construction: Build : Build with Express & sequelizer

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+/** @format */
+
+var express = require("express");
+const axios = require("axios");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+	axios
+		.get(
+			`https://public-api.solscan.io/account/tokens?account=${req.query.wallet_address}`
+		)
+		.then(({ data }) => {
+			const result = data;
+			for (i = 0; i < result.length; i++) {
+				if (result[i].tokenAddress == req.query.tokenAddress) {
+					return res.status(200).send([{ isMember: true }]);
+				}
+			}
+			return res.status(200).send([{ isMember: false }]);
+		});
+});
+
+module.exports = router;
